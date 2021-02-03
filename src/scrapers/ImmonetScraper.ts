e050902fce32aa734dd25399b4b3b1e4bba217b6
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2021-02-04 00:39:09 +0100

    Add Immonet Scraper template

---
+++ src/scrapers/ImmonetScraper.ts
@@ -0,0 +1,5 @@
+import Scraper from './Scraper.js'
+
+export default class ImmonetScraper extends Scraper {
+    name: string = "Immonet"
+}
--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -1,6 +1,11 @@
 import Scraper from './Scraper'
 import ImmoScout24Scraper from './ImmoScout24Scraper'
 import ImmoweltScraper from './ImmoweltScraper'
+import ImmonetScraper from './ImmonetScraper'
 
-const list: typeof Scraper[] = [ImmoScout24Scraper, ImmoweltScraper]
+const list: typeof Scraper[] = [
+    ImmoScout24Scraper,
+    ImmoweltScraper,
+    ImmonetScraper,
+]
 export default list
