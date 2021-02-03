44d09b4da270721c35f35cfa9ab4d1f4306d1516
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2021-02-03 23:02:18 +0100

    Fix error in pathname resolution

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import path from 'path'
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('live'))
+  .get('/', (req, res) => res.send('web scraper is alive'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
