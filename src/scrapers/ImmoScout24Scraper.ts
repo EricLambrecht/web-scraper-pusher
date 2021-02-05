f52419950e6838829213871faf7b18c958f157e8
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2021-02-05 20:47:34 +0100

    Add FIXME comment to immoscout scraper

--- src/scrapers/ImmoScout24Scraper.ts
+++ src/scrapers/ImmoScout24Scraper.ts
@@ -7,6 +7,8 @@ export default class ImmoScout24Scraper extends ListChangeScraper {
     'https://www.immobilienscout24.de/Suche/de/hamburg/hamburg/wohnung-mieten?numberofrooms=2.5-5.0&price=400.0-1260.0&livingspace=60.0-150.0&pricetype=rentpermonth&geocodes=0200000004045,0200000004051,0200000001040,0200000005054,0200000004037,0200000006075,0200000007070,0200000006052,0200000006055,0200000001047,0200000006073,0200000005056,0200000005053,0200000004069,0200000004049,0200000006059,0200000007076,0200000001041,0200000006057,0200000004046,0200000005048,0200000001039,0200000004068,0200000006074,0200000006058&sorting=2'
 
   async scrapeForListElement() {
+    // FIXME: Unfortunately Immoscout 24 uses a captcha to detect bots before displaying the result list
+    // So this will never work... :(
     return this.page.waitForSelector('ul#resultListItems')
   }
 
